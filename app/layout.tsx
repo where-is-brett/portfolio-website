d872d94e223ecb6b0a54bb0921f489d0c8a5d9e4
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-08-04 02:12:56 +1000

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import { Raleway } from 'next/font/google'
+import Head from 'next/head'
 
-const raleway = Raleway({ 
+const raleway = Raleway({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-raleway',
@@ -40,6 +41,27 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="snap-mandatory lg:snap-y lg:scroll-py-8">
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className={`${raleway.className} ${raleway.variable} ${bergen.variable} ${redtone.variable}`}>
         <nav>
           <Navbar breakpoint={1024} />
