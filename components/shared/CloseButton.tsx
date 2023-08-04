8ae75064576189d00f6ddfc471c2df42d3478b89
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-08-04 22:49:16 +1000

    Update CloseButton.tsx

--- components/shared/CloseButton.tsx
+++ components/shared/CloseButton.tsx
@@ -15,7 +15,7 @@ const CloseButton: React.FC<CloseButtonProps> = ({
       className='absolute right-4 top-4 z-50 border-0 bg-transparent p-0'
       title='Close menu'
     >
-      <Image src={CloseIcon} alt='close icon: x mark' height='30' width='30' />
+      <Image src={CloseIcon} alt='close icon: x mark' height='30' width='30' draggable={false} />
     </button>
   );
 };
