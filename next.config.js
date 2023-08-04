574163c6e0877124436168ae62b7dec8f0fa16cb
B._.rett <79561827+where-is-brett@users.noreply.github.com>
2023-08-04 13:16:58 +1000

    Update metadata

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/about',
+                permanent: false,
+            },
+        ]
+    }, // temporary - remove once home page has been added
     images: {
         domains: ['images.unsplash.com'],
     },
